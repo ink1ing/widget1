2c9ed6f206a4b5f8b48fa9cc7c326b05352a6d3f
ink1ing <inkling@proton.me>
2025-09-19 16:23:39 +0800

    Initial Commit

---
+++ Widget1/Widget1App.swift
@@ -0,0 +1,17 @@
+//
+//  Widget1App.swift
+//  Widget1
+//
+//  Created by INKLING on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Widget1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
